74de0ee3e59b9d60ff7b7bbcf4a28ebd655befb8
Deep Patel <56002273+deep882001@users.noreply.github.com>
2024-12-08 18:56:04 -0800

    Add files via upload

---
+++ Back-end/app/config.py
@@ -0,0 +1,12 @@
+DB_config = {
+    "host": "localhost",
+    "database": "asset_tracking",
+    "user": "postage",
+    "password": "Pat48830"
+}
+
+API_CONFIG = {
+    "api_url": "https://services.spireon.com/v0/rest/assets",
+    "api_token": "803ea778-2d93-4824-a9a6-19fecd598da3"
+    
+}
